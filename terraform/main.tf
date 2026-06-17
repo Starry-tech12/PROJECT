6542f5cf4cb2e7323e4b408283ab4ef24ba46888
Esther Ayo Isaiah <esther.ayo.isaiah@example.com>
2026-06-17 22:49:12 +0100

    fix: adjust min_size scaling config to allow cluster hibernation

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,9 +55,9 @@ module "eks" {
   eks_managed_node_groups = {
     main = {
       instance_types = ["t3.small"]
-      min_size     = 2
-      max_size     = 3
-      desired_size = 2
+      min_size     = 0
+      max_size     = 2
+      desired_size = 0
     }
   }
   
