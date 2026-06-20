87bb71e2bb4b10a4d7a8e092487b20874cdb2686
Esther Ayo Isaiah <esther.ayo.isaiah@example.com>
2026-06-20 12:31:16 +0100

    feat: establish error-free automated bedrock execution parameters

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "bedrock-assets-alt-soe-025-4138"
-#     key     = "capstone/project-bedrock/terraform.tfstate"
-#     region  = "us-east-1"
-#     encrypt = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket  = "bedrock-assets-alt-soe-025-4138"
+    key     = "capstone/project-bedrock/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+ }
